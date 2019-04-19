47f96b37d83f3573b681dc8e7b4696b63226d52b
Rachel Gould <rachelgould@Rachels-MacBook-Pro.local>
2019-04-19 14:27:06 -0700

    simplified solution

--- counting-valleys.js
+++ counting-valleys.js
@@ -2,34 +2,25 @@
 function countingValleys(n, s) {
   let stepArray = s.split('');
   let numValleys = 0;
-  const findFeatureEndIndex = (arr) => {
-    let up = 0;
-    let down = 0;
-    const incrementTerrain = (step) => {
-      if (step === 'U') {
-        up++
-        return up
-      } else {
-        down++
-        return down
+  let level = 0;
+  let valley = false;
+  for (let i = 0; i < stepArray.length; i++) {
+    if (stepArray[i] === 'U') {
+      level++
+      if (level === 1) {
+        valley = false;
       }
-    }
-    for (let i = 0; i < arr.length; i++) {
-      incrementTerrain(arr[i]);
-      if (up === down) {
-        return i
-      }
-    }
-  }
-  while (stepArray.length > 0) {
-    if (stepArray[0] === 'U') {
-      stepArray.splice(0, findFeatureEndIndex(stepArray) + 1);
     } else {
-      stepArray.splice(0, findFeatureEndIndex(stepArray) + 1);
-      numValleys++;
+      level--
+      if (level === -1) {
+        valley = true;
+      }
+    }
+    if (level === 0 && valley) {
+      numValleys++
     }
   }
   return numValleys;
 }
 
-console.log(countingValleys(100,"DUDUUUUUUUUDUDDUUDUUDDDUUDDDDDUUDUUUUDDDUUUUUUUDDUDUDUUUDDDDUUDDDUDDDDUUDDUDDUUUDUUUDUUDUDUDDDDDDDDD"))
+console.log(countingValleys(12,"DDUUDDUDUUUD"))
